cfc8fa9c852e534286d2e27a0c9c713af656ddc5
wchandler2020 <william.d.chandler1@gmail.com>
2025-08-31 21:18:30 -0500

    added: variants for each product

--- product/views.py
+++ product/views.py
@@ -1,8 +1,22 @@
+# from rest_framework import generics, permissions
+# from .models import Product
+# from .serializers import ProductSerializer
+
+# class ProductListView(generics.ListAPIView):
+#     queryset = Product.objects.filter(is_available=True).prefetch_related('variants')
+#     serializer_class = ProductSerializer
+#     permission_classes = [permissions.IsAuthenticated]
+    
 from rest_framework import generics, permissions
-from .models import Product
+from django.db.models import Prefetch
+from .models import Product, ProductVariant
 from .serializers import ProductSerializer
 
 class ProductListView(generics.ListAPIView):
-    queryset = Product.objects.filter(is_available=True)
     serializer_class = ProductSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Product.objects.filter(is_available=True).prefetch_related(
+            Prefetch('variants', queryset=ProductVariant.objects.filter(is_available=True))
+        )
