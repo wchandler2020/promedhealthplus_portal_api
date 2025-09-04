a20deeb10782dbb65d99e9b2789c055380df053b
wchandler2020 <william.d.chandler1@gmail.com>
2025-09-04 13:53:03 -0500

    fixed: fixed verify email url

--- provider_auth/urls.py
+++ provider_auth/urls.py
@@ -7,12 +7,12 @@ from provider_auth import views as api_views
 urlpatterns = [
     path('provider/token/', api_views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('provider/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('provider/register/', api_views.RegisterUser.as_view(), name='register'),
+    path('provider/register/', api_viewspath('verify-email/<uuid:token>/', api_views.VerifyEmailView.as_view(), name='verify-email'),.RegisterUser.as_view(), name='register'),
 #    path('send-code/', api_views.SendVerificationCodeView.as_view(), name='send-code'),
     path('verify-code/', api_views.VerifyCodeView.as_view(), name='verify-code'),
     path('provider/profile/', api_views.ProviderProfileView.as_view(), name='provider-profile'),
     path('provider/contact-rep/', api_views.ContactRepView.as_view(), name='contact-rep'),
-    path('verify-email/<uuid:token>/', api_views.VerifyEmailView.as_view(), name='verify-email'),
+    path('provider/verify-email/<uuid:token>/', api_views.VerifyEmailView.as_view(), name='verify-email'),
     path('provider/reset-password/<uuid:token>/', api_views.ResetPasswordView.as_view(), name='reset-password'),
     path('provider/request-password-reset/', api_views.RequestPasswordResetView.as_view(), name='request-password-reset'), 
 ]
