2a670d5f424fc1252de6f73d0db587f397b2fd71
wchandler2020 <william.d.chandler1@gmail.com>
2025-09-04 13:53:03 -0500

    fixed: fixed verify email url

--- provider_auth/views.py
+++ provider_auth/views.py
@@ -85,7 +85,7 @@ class RegisterUser(generics.CreateAPIView):
         token, created = EmailVerificationToken.objects.get_or_create(user=user)
 
         verification_link = f"https://wchandler2020.github.io/promedhealthplus_portal_client/#/verify-email/{token.token}"
-
+        
         email_html_message = render_to_string(
             'provider_auth/email_verification.html',
             {
