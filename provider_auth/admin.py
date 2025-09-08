a0c52bdc9401928d4a9d36ee62f8a755c2c1d9ac
wchandler2020 <william.d.chandler1@gmail.com>
2025-09-08 11:02:08 -0500

    fixed: user-admin removed date_joined

--- provider_auth/admin.py
+++ provider_auth/admin.py
@@ -1,24 +1,35 @@
 from django.contrib import admin
 from .models import User, Profile
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     list_display = (
         'email', 
         'full_name', 
-        'npi_number', # Add npi_number here
+        'npi_number',
         'is_verified', 
-        'is_approved', # Add is_approved here
+        'is_approved',
         'is_active', 
         'is_staff',
     )
     search_fields = ('email', 'full_name', 'npi_number')
     list_filter = ('is_verified', 'is_approved', 'is_staff', 'is_active')
-    
+
+    readonly_fields = ('date_joined',)
+
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('full_name', 'phone_number', 'npi_number')}), # Add npi_number here
-        ('Permissions', {'fields': ('is_active', 'is_approved', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}), # Add is_approved here
+        ('Personal info', {'fields': ('full_name', 'phone_number', 'npi_number')}),
+        ('Permissions', {
+            'fields': (
+                'is_active', 
+                'is_approved', 
+                'is_staff', 
+                'is_superuser', 
+                'groups', 
+                'user_permissions'
+            )
+        }),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
+
