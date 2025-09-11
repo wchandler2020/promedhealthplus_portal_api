e7fb45a3c7391a3a100c9f468265e6efcb8752b6
wchandler2020 <william.d.chandler1@gmail.com>
2025-09-11 12:31:34 -0500

    fixed: mass notification issue

--- notifications/views.py
+++ notifications/views.py
@@ -5,15 +5,20 @@ from rest_framework.response import Response
 from rest_framework.exceptions import PermissionDenied
 import notifications.serializers as api_serializers
 import notifications.models as api_models
-
-
-
+from provider_auth.models import User
+from django.utils import timezone
+from django.db import models
+        
 class NotificationListCreateView(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = api_serializers.NotificationSerializer
     
     def get_queryset(self):
-        return api_models.Notification.objects.filter(recipient=self.request.user)
+        return api_models.Notification.objects.filter(
+        recipient=self.request.user,
+    ).filter(
+        models.Q(expires_at__isnull=True) | models.Q(expires_at__gt=timezone.now())
+    )
     
 class MarkNotificationReadView(generics.UpdateAPIView):
     permission_classes = [permissions.IsAuthenticated]
@@ -44,3 +49,30 @@ class NotificationDeleteView(generics.DestroyAPIView):
         if instance.recipient != self.request.user:
             raise PermissionDenied("You do not have permission to delete this notification.")
         instance.delete()
+
+class BroadcastNotificationView(APIView):
+    permission_classes = [permissions.IsAdminUser]  # Only admins or authorized users
+
+    def post(self, request):
+        message = request.data.get('message')
+        notif_type = request.data.get('type', 'announcement')
+
+        if not message:
+            return Response({'error': 'Message field is required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        all_users = User.objects.all()
+
+        notifications = [
+            api_models.Notification(
+                recipient=user,
+                message=message,
+                type=notif_type,
+                broadcast=True
+            )
+            for user in all_users
+        ]
+
+        api_models.Notification.objects.bulk_create(notifications)
+
+        return Response({'detail': f'Notification sent to {len(notifications)} users.'}, status=status.HTTP_201_CREATED)
+
