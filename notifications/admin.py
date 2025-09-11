666de62dba06af165a244a110c6735ab4225930c
wchandler2020 <william.d.chandler1@gmail.com>
2025-09-11 12:31:34 -0500

    fixed: mass notification issue

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,5 +1,37 @@
+# notifications/admin.py
 from django.contrib import admin
+from django import forms
 from .models import Notification
+from provider_auth.models import User
 
-# Register your models here.
-admin.site.register(Notification)
+class NotificationAdminForm(forms.ModelForm):
+    send_to_all = forms.BooleanField(required=False, help_text="Check to send this notification to all users.")
+
+    class Meta:
+        model = Notification
+        fields = '__all__'
+
+class NotificationAdmin(admin.ModelAdmin):
+    form = NotificationAdminForm
+
+    def save_model(self, request, obj, form, change):
+        send_to_all = form.cleaned_data.get('send_to_all')
+        if send_to_all:
+            users = User.objects.all()
+            notifications = [
+                Notification(
+                    recipient=user,
+                    message=obj.message,
+                    type=obj.type,
+                    data=obj.data,
+                    broadcast=True
+                )
+                for user in users
+            ]
+            Notification.objects.bulk_create(notifications)
+        else:
+            # Save normally for one recipient
+            super().save_model(request, obj, form, change)
+            
+admin.site.register(Notification, NotificationAdmin)
+            
--- notifications/urls.py
+++ notifications/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import NotificationListCreateView, MarkNotificationReadView, UnreadNotificationCountView, NotificationDeleteView
+from .views import NotificationListCreateView, MarkNotificationReadView, UnreadNotificationCountView, NotificationDeleteView, BroadcastNotificationView
 
 urlpatterns = [
     path('provider/notifications/', NotificationListCreateView.as_view(), name='notification-list-create'),
     path('unread-count/', UnreadNotificationCountView.as_view(), name='notification-unread-count'),
     path('<int:pk>/mark-read/', MarkNotificationReadView.as_view(), name='notification-mark-read'),
     path('<int:pk>/delete-notification/', NotificationDeleteView.as_view(), name='notification-delete'),
+    path('provider/notifications/broadcast/', BroadcastNotificationView.as_view(), name='notifications-broadcast'),
 ]
 
 
