e620b2318780d274967a714ac58a265b751fee97
wchandler2020 <william.d.chandler1@gmail.com>
2025-09-16 15:13:03 -0500

    added: patient edit and delete ops to patients/views.py

--- patients/urls.py
+++ patients/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from patients import views as api_views
 urlpatterns = [
     path('patients/', api_views.PatientListView.as_view(), name='patient-list'),
+    path('patients/<int:pk>/', api_views.PatientDetailView.as_view(), name='patient-detail'),
 ]
