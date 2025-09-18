e2c0de2d3a9d5fb031d4957af42a3cefcaba0a9a
wchandler2020 <william.d.chandler1@gmail.com>
2025-09-18 10:30:16 -0500

    added: roles to Users

--- provider_auth/admin.py
+++ provider_auth/admin.py
@@ -1,11 +1,14 @@
+# provider_auth/admin.py
 from django.contrib import admin
 from .models import User, Profile
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     list_display = (
         'email', 
         'full_name', 
+        'role',  # ⬅️ Add role to list_display for an overview
         'npi_number',
         'is_verified', 
         'is_approved',
@@ -13,7 +16,7 @@ class UserAdmin(BaseUserAdmin):
         'is_staff',
     )
     search_fields = ('email', 'full_name', 'npi_number')
-    list_filter = ('is_verified', 'is_approved', 'is_staff', 'is_active')
+    list_filter = ('is_verified', 'is_approved', 'is_staff', 'is_active', 'role') # ⬅️ Add role to list_filter
 
     readonly_fields = ('date_joined',)
 
@@ -22,6 +25,7 @@ class UserAdmin(BaseUserAdmin):
         ('Personal info', {'fields': ('full_name', 'phone_number', 'npi_number')}),
         ('Permissions', {
             'fields': (
+                'role', # ⬅️ Add role here so it is editable in the admin
                 'is_active', 
                 'is_approved', 
                 'is_staff', 
@@ -34,4 +38,3 @@ class UserAdmin(BaseUserAdmin):
     )
     
 admin.site.register(Profile)
-
