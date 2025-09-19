f880ed9d27d5536282c265ff8a8169b51fb020cf
wchandler2020 <william.d.chandler1@gmail.com>
2025-09-19 10:53:24 -0500

    fixed: error in patient serializer

--- patients/serializers.py
+++ patients/serializers.py
@@ -4,12 +4,6 @@ class PatientSerializer(serializers.ModelSerializer):
     provider = serializers.PrimaryKeyRelatedField(read_only=True)  # Can be replaced with a nested serializer
     class Meta:
         model = Patient
-        fields = [
-            'id',
-            'full_name',
-            'dob',
-            'gender',
-            'provider',
-            'date_created',
-            'date_updated',
-        ]
+        fields = '__all__'
+        
+
