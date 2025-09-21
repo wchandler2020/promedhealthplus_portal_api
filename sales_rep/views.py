71a1e8dad5803b17fa396b5a81e36326035955d3
wchandler2020 <william.d.chandler1@gmail.com>
2025-09-21 09:55:23 -0500

    updated: sales_rep/serializer.py to return stats for orders and IVRs statuses

--- sales_rep/views.py
+++ sales_rep/views.py
@@ -4,14 +4,14 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 
 from sales_rep.models import SalesRep
-from .serializers import SalesRepDashboardSerializer  # <-- Import the correct serializer
+from .serializers import SalesRepDashboardSerializer
 
 class SalesRepDashboardView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
         try:
-            sales_rep = request.user.salesrep_profile  # ensure this is the SalesRep object
+            sales_rep = request.user.salesrep_profile
         except SalesRep.DoesNotExist:
             return Response(
                 {"error": "You are not associated with a SalesRep profile."},
